3b8b0a893baf0eeba557b87dc9ce15664004c5c0
Stefan Ilic <stefanilic952@gmail.com>
2018-12-28 23:40:28 +0100

    Initial commit

---
+++ Netto To Brutto - WebApp/App_Code/RouteConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Routing;
+using Microsoft.AspNet.FriendlyUrls;
+
+namespace Netto_To_Brutto___WebApp
+{
+    public static class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            var settings = new FriendlyUrlSettings();
+            settings.AutoRedirectMode = RedirectMode.Permanent;
+            routes.EnableFriendlyUrls(settings);
+        }
+    }
+}
---
+++ Netto To Brutto - WebApp/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Netto_To_Brutto___WebApp.Startup))]
+namespace Netto_To_Brutto___WebApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
